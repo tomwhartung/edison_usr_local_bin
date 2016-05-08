34db229f2bb4366d85f64f5d6bf509aeb5963629
Tom W Hartung <tomwhartung@gmail.com>
2016-05-08 14:20:15 -0600

    Adding tomsUlbFunctions.py which defines functions useful for more than one program in /usr/local/bin .

---
+++ bin/pythonLibs/tomsUlbFunctions.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+#
+# tomsUlbFunctions.py: define functions useful for more than one program in /usr/local/bin
+# ----------------------------------------------------------------------------------------
+#
+from subprocess import call
+import os       # for getting values for environment vars
+import sys      # for accessing command line arguments
+import time     # for the date string in our backup file name
+import getopt   # for processing command line options
+
+##
+#  Wrapper function to run a command
+#
+def myCall( shellCommand ) :
+	call( shellCommand, shell=True )
+
+##
+#  Determine whether we are using Yocto Linux (on the Edison)
+#  If not, assume debian
+#
+def isYoctoLinux() :
+	return False
+
+def getMatchingProcesses( toMatch ) :
+	if ( isYoctoLinux() ) :
+		psCommandArgs = ''
+	else :
+		psCommandArgs = '-aef'
+	psCommand = 'ps ' + psCommandArgs
+	myCall( psCommand )
+
+#
+#  Process the command line arguments.
+#
+def processArguments( backupOrRestore ) :
+	siteArg = ''
+	suffixArg = ''
+	if ( len(sys.argv) == 2 ) :
+		if ( sys.argv[1] == '--help' ) :
+			syntax( backupOrRestore )
+			exit( 0 )
+		siteArg = sys.argv[1]
+	elif ( len(sys.argv) == 3 ) :
+		siteArg = sys.argv[1]
+		suffixArg = sys.argv[2]
+	elif ( backupOrRestore == 'command' ) :   # Really a trivial case (no file name involved)
+		siteArg = 'all'                        # TODO: implement in a future iteration
+	else :
+		syntax( backupOrRestore )
+		print( 'Wrong number of args.' )
+		print( 'len(sys.argv) =', len(sys.argv) )
+		print( 'str(sys.argv) =', str(sys.argv) )
+		exit( 1 )
+	arguments = [siteArg, suffixArg]
+	return arguments
+
+#
+#  Display a brief syntax statement, describing the options and purpose of the script
+#
+def syntax( backupOrRestore ) :
+	restoreOptions = ''
+	if( backupOrRestore == 'restore' ) :
+		restoreOptions = '[-d YYYY_MM_DD] [-h hostName]'
+	basename = os.path.basename( sys.argv[0] )
+	print( 'Syntax:' )
+	print( '  ' + basename + ' [--help] ' + restoreOptions + ' site [suffix]' )
+	if( backupOrRestore == 'restore' ) :
+		print( '    -d: optionally override today\'s date in fileName (YYYY_MM_DD)' )
+		print( '    -h: optionally override current hostName in fileName' )
+	print( '    --help: display syntax statement then exit' )
+	print( '    site: site name or recognized abbreviation' )
+	print( '    suffix: optional string appended to standard backup file name' )
+	if( backupOrRestore == 'backup' ) :
+		print( 'Saves a copy of database in a file named as follows:' )
+	else :
+		print( 'Restores a copy of database from a file named as follows:' )
+	print( '  siteName-YYYY_MM_DD-hostname[-suffix].sql.gz' )
+
